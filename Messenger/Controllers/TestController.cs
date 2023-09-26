42d29d5e1ef2bfdfe293b9dd0aed212026fc1dcc
Mylov Vasily <zarkaria.dia@gmail.com>
2023-09-26 23:38:42 +0400

    TestController returns "Hello World!" now

--- Messenger/Controllers/TestController.cs
+++ Messenger/Controllers/TestController.cs
@@ -14,14 +14,5 @@ public class TestController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-    }
+    public string HelloWorld() => "Hello World!";
 }
